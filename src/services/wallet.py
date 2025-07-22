c7328f1dda0394fc284ca5eeab94ed4ebaa1744c
Nadorozhnyy <59502289+Nadorozhnyy@users.noreply.github.com>
2025-07-22 16:27:19 +0300

    add asyncio lock for operation with wallet

--- src/services/wallet.py
+++ src/services/wallet.py
@@ -1,4 +1,7 @@
+import asyncio
 from uuid import UUID
+from typing import Dict
+from contextlib import asynccontextmanager
 
 from src.models.wallet import OperationTypeEnum
 from src.schemas import OperationWalletSchema
@@ -8,6 +11,20 @@ from src.exceptions.wallet import WalletNotFound
 
 
 class WalletService(AppServices):
+    _locks: Dict[UUID, asyncio.Lock] = {}
+    _lock_for_locks = asyncio.Lock()
+
+    async def _get_or_create_lock(self, wallet_id: UUID):
+        async with self._lock_for_locks:
+            if wallet_id not in self._locks:
+                self._locks[wallet_id] = asyncio.Lock()
+            return self._locks[wallet_id]
+
+    @asynccontextmanager
+    async def _wallet_lock(self, wallet_id):
+        lock = await self._get_or_create_lock(wallet_id)
+        async with lock:
+            yield
 
     async def _get_wallet_or_raise(self, wallet_id: UUID):
         wallet = await WalletCrud(self.db).get_wallet_by_id(wallet_id)
@@ -23,12 +40,13 @@ class WalletService(AppServices):
         return ServiceResult(wallet)
 
     async def operation_with_wallet_by_id(self, wallet_id: UUID, operation_data: OperationWalletSchema):
-        wallet = await self._get_wallet_or_raise(wallet_id)
+        async with self._wallet_lock(wallet_id):
+            wallet = await self._get_wallet_or_raise(wallet_id)
 
-        if operation_data.operation_type == OperationTypeEnum.DEPOSIT:
-            wallet.amount += operation_data.amount
-        elif operation_data.operation_type == OperationTypeEnum.WITHDRAW:
-            wallet.amount -= operation_data.amount
+            if operation_data.operation_type == OperationTypeEnum.DEPOSIT:
+                wallet.amount += operation_data.amount
+            elif operation_data.operation_type == OperationTypeEnum.WITHDRAW:
+                wallet.amount -= operation_data.amount
 
-        update_wallet = await WalletCrud(self.db).update_wallet(wallet)
-        return ServiceResult(update_wallet)
+            update_wallet = await WalletCrud(self.db).update_wallet(wallet)
+            return ServiceResult(update_wallet)
