0c60ff178e043b7c43d687f61209a1a120064bb0
Nadorozhnyy <59502289+Nadorozhnyy@users.noreply.github.com>
2025-07-23 13:43:43 +0300

    fix tests

--- tests/wallet/test_wallet.py
+++ tests/wallet/test_wallet.py
@@ -1,5 +1,5 @@
 import pytest
-from uuid import uuid4
+from uuid import uuid4, UUID
 from fastapi import status
 from httpx import AsyncClient
 
@@ -13,7 +13,7 @@ class TestWalletEndpoints:
     def wallet_uuid(self):
         return str(uuid4())
 
-    async def test_get_wallet_by_id_success(self, async_client: AsyncClient, wallet_uuid, mocker):
+    async def test_get_wallet_by_id_success(self, async_client: AsyncClient, wallet_uuid: UUID, mocker):
         wallet_data = {
             "id": str(wallet_uuid),
             "amount": 100
@@ -30,14 +30,14 @@ class TestWalletEndpoints:
         assert response_data["id"] == str(wallet_uuid)
         assert response_data["amount"] == 100
 
-    async def test_get_wallet_by_id_failed(self, async_client: AsyncClient, wallet_uuid, mocker):
+    async def test_get_wallet_by_id_failed(self, async_client: AsyncClient, wallet_uuid: UUID):
         invalid_uuid = "invalid-uuid"
 
         response = await async_client.get(f"/api/v1/wallets/{invalid_uuid}")
 
         assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
 
-    async def test_wallet_operation_deposit_success(self, async_client: AsyncClient, wallet_uuid, mocker):
+    async def test_wallet_operation_deposit_success(self, async_client: AsyncClient, wallet_uuid: UUID, mocker):
         operation_data = {
             "operation_type": "DEPOSIT",
             "amount": 100
@@ -60,7 +60,7 @@ class TestWalletEndpoints:
         assert response.json()["id"] == str(wallet_uuid)
         assert response.json()["amount"] == 200
 
-    async def test_wallet_operation_withdraw_success(self, async_client: AsyncClient, wallet_uuid, mocker):
+    async def test_wallet_operation_withdraw_success(self, async_client: AsyncClient, wallet_uuid: UUID, mocker):
         operation_data = {
             "operation_type": "DEPOSIT",
             "amount": 200
@@ -83,7 +83,7 @@ class TestWalletEndpoints:
         assert response.json()["id"] == str(wallet_uuid)
         assert response.json()["amount"] == 100
 
-    async def test_wallet_operation_amount_failed(self, async_client: AsyncClient, wallet_uuid):
+    async def test_wallet_operation_amount_failed(self, async_client: AsyncClient, wallet_uuid: UUID):
         operation_data = {
             "operation_type": "DEPOSIT",
             "amount": -50
@@ -96,7 +96,7 @@ class TestWalletEndpoints:
 
         assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
 
-    async def test_wallet_operation_amount_zero_failed(self, async_client: AsyncClient, wallet_uuid):
+    async def test_wallet_operation_amount_zero_failed(self, async_client: AsyncClient, wallet_uuid: UUID):
         operation_data = {
             "operation_type": "DEPOSIT",
             "amount": 0
@@ -109,7 +109,7 @@ class TestWalletEndpoints:
 
         assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
 
-    async def test_wallet_operation_operation_type_failed(self, async_client: AsyncClient, wallet_uuid):
+    async def test_wallet_operation_operation_type_failed(self, async_client: AsyncClient, wallet_uuid: UUID):
         operation_data = {
             "operation_type": "WRONG_TYPE",
             "amount": 100
